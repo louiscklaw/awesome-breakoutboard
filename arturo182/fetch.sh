143aed550c1dabe4c9f65f68f1e1cb1a3d7eb64c
Travis CI <travis@travis-ci.org>
2019-12-16 19:55:11 +0800

    update,

---
+++ arturo182/fetch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+git submodule add --depth=1 https://github.com/arturo182/3.95_dsi_lcd_breakout
+git submodule add --depth=1 https://github.com/arturo182/bbq10kbd_breakout
+git submodule add --depth=1 https://github.com/arturo182/cnc_featherwing
+git submodule add --depth=1 https://github.com/arturo182/fuel_gauge_charger_breakout
+git submodule add --depth=1 https://github.com/arturo182/IS31FL3741_breakout
+git submodule add --depth=1 https://github.com/arturo182/libretto_keyboard_breakout
+git submodule add --depth=1 https://github.com/arturo182/mosfet_breakout
+git submodule add --depth=1 https://github.com/arturo182/pam8301_breakout
+git submodule add --depth=1 https://github.com/arturo182/pmod_ov2640_camera
+git submodule add --depth=1 https://github.com/arturo182/pmod_sgtl5000_breakout
+git submodule add --depth=1 https://github.com/arturo182/sama5d27c_breakout
+git submodule add --depth=1 https://github.com/arturo182/samd09_breakout
+git submodule add --depth=1 https://github.com/arturo182/samd21_breakout
+git submodule add --depth=1 https://github.com/arturo182/samd51_breakout
+git submodule add --depth=1 https://github.com/arturo182/sams70_breakout
+git submodule add --depth=1 https://github.com/arturo182/stusb1600_breakout
+
+wait
