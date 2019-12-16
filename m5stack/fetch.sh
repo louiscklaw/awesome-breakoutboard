be41f9d680e1159baf57a6410b65377c1cec96b5
Travis CI <travis@travis-ci.org>
2019-12-16 22:52:33 +0800

    update,

--- m5stack/fetch.sh
+++ m5stack/fetch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-git submodule add --depth=1 git@github.com:m5stack/M5-Schematic.git &
-git submodule add --depth=1 git@github.com:m5stack/M5-3D_and_PCB.git &
-git submodule add --depth=1 git@github.com:m5stack/M5Stack_TFT_ILI9341.git &
+git submodule add --depth=1 -f https://github.com/m5stack/M5-Schematic.git
+git submodule add --depth=1 -f https://github.com/m5stack/M5-3D_and_PCB.git
+git submodule add --depth=1 -f https://github.com/m5stack/M5Stack_TFT_ILI9341.git
 
 wait
