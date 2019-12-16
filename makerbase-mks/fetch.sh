3dae9dc38ccac94be79b8589fdd61570c66dc2d0
Travis CI <travis@travis-ci.org>
2019-12-16 20:09:51 +0800

    init commit add makerbase-mks,

---
+++ makerbase-mks/fetch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+git submodule add --depth=1 https://github.com/makerbase-mks/MKS-Robin-Pro.git
+git submodule add --depth=1 https://github.com/makerbase-mks/MKS-Robin-mini.git
+git submodule add --depth=1 https://github.com/makerbase-mks/MKS-Robin-lite3.git
+git submodule add --depth=1 https://github.com/makerbase-mks/MKS-Robin-Nano.git
+git submodule add --depth=1 https://github.com/makerbase-mks/MKS-SERVO42B.git
+git submodule add --depth=1 https://github.com/makerbase-mks/MKS-SERVO57A.git
+git submodule add --depth=1 https://github.com/makerbase-mks/MKS-SERVO42A.git
+
+
+wait
