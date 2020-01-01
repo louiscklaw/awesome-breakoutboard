4d1ee90f7b9009127f4ead26230760acdc3f6446
Travis CI <travis@travis-ci.org>
2020-01-01 18:09:16 +0800

    update for shallow script,

--- _utils/shallow_submodule.py
+++ _utils/shallow_submodule.py
@@ -7,13 +7,14 @@ cwd = os.path.dirname(os.path.abspath(__file__))
 gitmodules_path = os.path.sep.join([cwd,'..','.gitmodules'])
 
 with open(gitmodules_path, 'r') as fi:
+  print('start shallow ing repo')
   try:
     for line in fi.readlines():
       if line.find('path = ') > -1:
         submodule_path.append(line.strip().split('path = ')[1])
 
     for command_path in submodule_path:
-      print('running shallow command for path {}'.format(command_path))
+      # print('running shallow command for path {}'.format(command_path))
       os.system('git config -f .gitmodules submodule.{}.shallow true'.format(command_path))
 
     sys.exit(0)
@@ -21,3 +22,5 @@ with open(gitmodules_path, 'r') as fi:
   except Exception as e:
     print('error found during settings shallow submodules ...')
     sys.exit(1)
+
+  print('shallowing repo done')
