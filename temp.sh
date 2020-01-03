e08c1dce0e6e2dee06c3e56705f65253debe3597
Travis CI <travis@travis-ci.org>
2020-01-03 11:29:14 +0800

    update for feature/makerbase-mks-MKS-TB6600,

--- temp.sh
+++ temp.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
 
 
-git submodule add --depth=1 https://github.com/david-pzh/X-Bot.git david-pzh/X-Bot
-git submodule add --depth=1 https://github.com/david-pzh/DoubiBot.git david-pzh/DoubiBot
-git submodule add --depth=1 https://github.com/david-pzh/Roboard-Pro.git david-pzh/Roboard-Pro
-
+git submodule add --depth=1 https://github.com/makerbase-mks/MKS-TB6600.git makerbase-mks/MKS-TB6600
 
 python3 _utils/shallow_submodule.py
 
